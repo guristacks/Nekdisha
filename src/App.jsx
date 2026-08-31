447c6ebcf9e7a32df11129c7d7cf3218884df306
Guri Ghumaan <guristacks@gmail.com>
2026-08-31 16:03:20 +0530

    Fix import path for Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Footer from "./Components/Footer/hooter";
+import Footer from "./Components/Footer/footer";
 import Header from "./Components/Header/header";
 import Hero from "./Components/Hero/hero";
 
