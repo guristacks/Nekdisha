9a6c9020275550ab6ff1e7ba31e1bbf4fc49558c
Guri Ghumaan <guristacks@gmail.com>
2026-08-31 15:59:47 +0530

    Add Header component with navigation and call-to-action

---
+++ src/Components/Header/header.jsx
@@ -0,0 +1,42 @@
+import { ChevronsRight } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="headContent flex ac sb">
+          <a className="flex ac" href="">
+            <img src="favicon.png" alt="" /> <p>Nek Disha</p>
+          </a>
+          <nav>
+            <ul className="flex ac">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Donation</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="cta flex ac">
+            <div className="svg">
+              <ChevronsRight color="#ff3c00" />
+            </div>
+            <a href="#">Join with us</a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
