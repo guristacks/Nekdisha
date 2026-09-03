2069d21e82e7f6c0a19c034433ae329653c5c987
Guri Ghumaan <guristacks@gmail.com>
2026-09-03 11:43:17 +0530

    Refactor App component imports and clean up unused CSS styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import Footer from "./Components/Footer/footer";
-import Header from "./Components/Header/header";
-import Hero from "./Components/Hero/hero";
+import About from "./Components/Home-Page/About/about";
+import Footer from "./Components/Home-Page/Footer/footer";
+import Header from "./Components/Home-Page/Header/header";
+import Hero from "./Components/Home-Page/Hero/hero";
 
 const App = () => {
   return (
     <>
       <Header />
       <Hero />
+      <About />
       <Footer />
     </>
   );
