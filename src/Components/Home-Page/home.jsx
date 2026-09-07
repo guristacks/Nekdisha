191c5606f2381b3aae367e3a60e08ab0a9861ff2
Guri Ghumaan <guristacks@gmail.com>
2026-09-07 07:05:52 +0530

    Add InstaPosts component to Home page and adjust container width for better layout

--- src/Components/Home-Page/home.jsx
+++ src/Components/Home-Page/home.jsx
@@ -2,6 +2,7 @@ import About from "./About/about";
 import Footer from "./Footer/footer";
 import Header from "./Header/header";
 import Hero from "./Hero/hero";
+import Insta from "./InstaPosts/insta";
 import Join from "./JoinUs/join";
 import Programs from "./Programs/programs";
 import Stats from "./Stats/stats";
@@ -15,6 +16,7 @@ const Home = () => {
       <Stats />
       <Programs />
       <Join />
+      <Insta />
       <Footer />
     </>
   );
